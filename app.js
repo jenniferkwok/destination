55b704aca6a4477b0e52411da48f83e5dbca4ad7
jenniferkwok <jenwkwok@gmail.com>
2015-12-13 15:51:58 -0800

    removed angular files

--- app.js
+++ app.js
@@ -4,56 +4,41 @@ var mongoose = require('mongoose');
 var db = require('./app/models/index.js');
 var request = require('request');
 var bodyParser = require('body-parser');
-var apiRouter = require('./app/config/routes'); // bring in API routes
 
 //CONFIG//
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // configure body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//mongoose.createConnection("mongodb://localhost/destination");
+// mongoose.createConnection("mongodb://localhost/destination");
 
 //ROUES//
-//app.get('*', routes.index);
-
-// apply router middleware
-// namespace your api
-app.use('/api', apiRouter);
-
-// serve static files
-app.use(express.static(__dirname + '/public'));
-
-// this is the entry way into the client-side
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
+app.get("/", function(req, res){
+	db.Post.find().exec(function(err, posts){
+		if (err){console.log("err! cannot get");}
+	res.render('index', {posts: posts});
+});
 });
 
-// app.get("/", function(req, res){
-// 	db.Post.find().exec(function(err, posts){
-// 		if (err){console.log("err! cannot get");}
-// 	res.render('index', {posts: posts});
-// });
-// });
+app.get("/api/posts", function(req, res){
+	db.Post.find(function(err, posts){
+		res.send(posts);
+	});
+});
 
-// app.get("/api/posts", function(req, res){
-// 	db.Post.find(function(err, posts){
-// 		res.send(posts);
-// 	});
-// });
-
-// app.post("/api/posts", function (req, res){
-// 	var newPost={
-// 		imgurl: req.body.imgurl,
-// 		location: req.body.location,
-// 		author:req.body.author
-// 	};
-// 	db.Post.create(newPost, function(err, post){
-// 		if (err){console.log("err! cannot create");}
-// 		res.json(post);
-// 	});
-//  });
+app.post("/api/posts", function (req, res){
+	var newPost={
+		imgurl: req.body.imgurl,
+		location: req.body.location,
+		author:req.body.author
+	};
+	db.Post.create(newPost, function(err, post){
+		if (err){console.log("err! cannot create");}
+		res.json(post);
+	});
+ });
 
 app.listen(process.env.PORT || 7000, function () {
   console.log('7000 GO GO GO');
--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,70 @@
-console.log("sanity check!!!");
+console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+
 		pageload();
-});
 
-		debugger
-
-function pageload(){
-	$('#submitnewpost').submit(function(e){
+//SIGNUP
+	$("#signup").submit(function(e){
 		e.preventDefault();
-		// console.log('New Post Created!',$(this).serialize() );
-		$.post("/api/posts", $(this).serialize(), function(response){
-		$('#submitnewpost')[0].reset();
+		$.post("/users", $("#signup").serialize(), function(response){
+			console.log(response);
+		});
+	});
+
+//LOGIN
+	$("#login").submit(function(e){
+		e.preventDefault();	
+		$.post("/login", $("#login").serialize(), function(response){
+		}).success(function(data){
+			// window.location=data;
+			window.location.href = "/profile";
+		});
+	});
+
+//SIGNOUT
+	// $("#logout").submit(function(e){
+	// 	e.preventDefault();	
+	// 	$.get("/logout", function(response){
+	// 	}).success(function(data){
+	// 		// window.location=data;
+	// 		window.location.href = "/index";
+	// 	});
+	// });
+
 });
+function pageload(){
+	$('#submitnewquote').submit(function(e){
+		e.preventDefault();
+		console.log('the serialize gives :',$(this).serialize() );
+		$.post("api/posts", $(this).serialize(), function(response){
+
+			// var qString = makeNewPost(response);
+			// $('#content').prepend(qString);
+		$('#submitnewquote')[0].reset();
+		$('#submitnewquote').append("<hr><h5>Thanks for your contribution!</h5");
+		});
+	});
+// function makeNewPost(newquote){
+// 	return '<section class="posts"><button class="deletebutton close">&times;</button><h3>“' + submitnewquote.quote.value + '”</h3><br><h5>- ' + submitnewquote.author.value + '</h5></section>';
+// }
+
+//EventListener for delte button
+$(document).on('click', '.deletebutton', function(e){
+	e.preventDefault();
+	deletePost(this);
 });
+}
+
+function deletePost(context){
+	var postId= $(context).data().id;
+	console.log(postId);
+	$.ajax({
+		url: 'api/posts/' + postId,
+		type: 'DELETE',
+		success: function(response){
+			$(context).closest('section').remove();
+		}
+	});
+}
+
